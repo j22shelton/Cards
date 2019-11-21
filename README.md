ff0d1fcea3c1602575a01574322ea843e88496ba
JoJo <shelton.jocelyn@gmail.com>
2019-11-20 22:50:08 -0500

    initial set up

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Cards
+
+## A fun and useful tool that allows a user to sign in and create their own personal input on the flip cards. 
+Created using HTML, CSS, Bootstrap, JQuery, and JavaScript.
+
+  Check it out at: https://j22shelton.github.io/Cards/
+  
---
+++ assets/JS/cards.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var recipes = ['Thai Stir Fry', 'Breaded Garlic Chicken', 'Stuffed Bell Peppers', 'Salmon', 'Korean Crack Slaw', 'Jambalaya', 'Thai Cashew Chicken', 'Red Lentil Curry', 'OnePan Mexican', 'Stri-Fry', 'Chilli', 'Fajitas'
+]
+    $("#random-button").on("click", function() {
+      var random = recipes[Math.floor(Math.random() * recipes.length)];
+      $("#random-recipe").text(random);
+    });
+
+});
